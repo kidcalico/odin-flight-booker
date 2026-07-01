b39ed1fb8f558b213d316a08bd79ff55b404023f
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-07-01 13:23:38 -0700

    Write initial seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Populating database..."
+
+airports = [ [ "SAN", "San Diego International Airport" ], [ "JFK", "John F. Kennedy International Airport" ], [ "BOM", "Mumbai International Airport" ] ]
+
+airports.each do |location|
+  Airport.create do |airport|
+    airport.name = location[1]
+    airport.code = location[0]
+  end
+end
+
+flights = [ { departure_time: "2026-07-05 08:30:00", departure_airport_id: Airport.find_by(code: "SAN").id, arrival_time: "2026-07-05 14:00:00", arrival_airport_id: Airport.find_by(code: "JFK").id, duration: 330 }, { departure_time: "2026-07-05 10:30:00", departure_airport_id: Airport.find_by(code: "SAN").id, arrival_time: "2026-07-05 16:00:00", arrival_airport_id: Airport.find_by(code: "JFK").id, duration: 330 }, { departure_time: "2026-07-05 16:30:00", departure_airport_id: Airport.find_by(code: "SAN").id, arrival_time: "2026-07-05 22:00:00", arrival_airport_id: Airport.find_by(code: "JFK").id, duration: 330 } ]
+
+flights.each do |hash|
+  Flight.create do |flight|
+    flight.departure_time = hash[:departure_time]
+    flight.arrival_time = hash[:arrival_time]
+    flight.departure_airport_id = hash[:departure_airport_id]
+    flight.arrival_airport_id = hash[:arrival_airport_id]
+    flight.duration = hash[:duration]
+  end
+end
+puts "Database seeded."
