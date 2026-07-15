12a70219af2c153cff3c9386f8d8f4baf3bbad27
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-07-15 15:40:35 -0700

    Create flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @airports = Airport.all.map { |a| [ a.name, a.code ] }
+    @dates = Flight.pluck(:departure_time).map { |datetime| datetime.to_date }.uniq
   end
 end
