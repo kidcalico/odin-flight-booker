65552f85d07d5d04d84be8ec36b10ed93091fb9d
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-07-17 15:18:24 -0700

    Implement search function
    Write controller method to update @flights with relevant data. Update
    view to display the relevant flights.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    # @flights = Flight.all
     @airports = Airport.all.map { |a| [ a.name, a.code ] }
     @dates = Flight.pluck(:departure_time).map { |datetime| datetime.to_date }.uniq
+
+    if params[:departure_airport].present?
+      departure_airport = Airport.find_by(code: params[:departure_airport])
+      arrival_airport = Airport.find_by(code: params[:arrival_airport])
+      date = params[:departure_time].to_date
+      @flights = Flight.where(departure_airport_id: departure_airport.id)
+                        .where(arrival_airport_id: arrival_airport.id)
+                        .where(departure_time: date.beginning_of_day..date.end_of_day)
+    end
   end
 end
