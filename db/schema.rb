26981393a9965cdf320931a690a94034d7f1d223
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-07-30 14:41:23 -0700

    Generate and migrate Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_30_214543) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_30_213918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,6 +31,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_30_214543) do
     t.datetime "updated_at", null: false
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
