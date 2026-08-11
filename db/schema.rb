91b8a0527f4034db1532650d2ef6f0de24238bbe
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-08-11 14:53:23 -0700

    Create and migrate Bookings, add ref to Passenger

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_30_213918) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_11_213800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_30_213918) do
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "flight_id", null: false
+    t.integer "passenger_count"
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -37,9 +45,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_30_213918) do
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
+    t.bigint "passenger_id", null: false
     t.datetime "updated_at", null: false
+    t.index ["passenger_id"], name: "index_passengers_on_passenger_id"
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "passengers"
 end
