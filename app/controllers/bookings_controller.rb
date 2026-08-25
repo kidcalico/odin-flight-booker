0d703bfeb22a2679087c7eb532174b523dd298f7
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-08-25 15:52:50 -0700

    Update BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,14 @@
 class BookingsController < ApplicationController
   def new
     @passenger_count = params[:passenger_count]
-    @flight = params[flight.id]
+    @flight_id = params[:flight_id]
+    @flight = Flight.find_by(id: params[:flight_id])
+    @passengers = []
+
+    @booking = Booking.new
+
+    @passenger_count.to_i.times do
+      @booking.passengers.build
+    end
   end
 end
